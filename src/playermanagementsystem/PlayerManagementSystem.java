903b7988db8e9afa24c7d42f7df2f4a6ab76ea9e
000000000min <youngmin0825@gmail.com>
2023-03-22 10:43:39 +0900

    계정확인

--- src/playermanagementsystem/PlayerManagementSystem.java
+++ src/playermanagementsystem/PlayerManagementSystem.java
@@ -8,6 +8,7 @@ public class PlayerManagementSystem {
 		System.out.println("Edit Player");
 		System.out.println("View Player");
 		System.out.println("Exit");
+		System.out.println("계정설정 확인용입니다.");
 
 	}
 
