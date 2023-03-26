8849283885bc694de0181403ebe5b41ff34c2e8d
000000000min <youngmin0825@gmail.com>
2023-03-26 22:21:23 +0900

    add menu repeatation

--- src/playermanagementsystem/PlayerManagementSystem.java
+++ src/playermanagementsystem/PlayerManagementSystem.java
@@ -1,14 +1,55 @@
 package playermanagementsystem;
 
+import java.util.Scanner;
+
 public class PlayerManagementSystem {
 
 	public static void main(String[] args) {
-		System.out.println("Add Player");
-		System.out.println("Delete Player");
-		System.out.println("Edit Player");
-		System.out.println("View Player");
-		System.out.println("Exit");
-		System.out.println("계정설정 확인용입니다.");
+		
+		int num = 0;
+		Scanner input = new Scanner(System.in);
+		
+		while(num != 6) {
+			System.out.println("1. Add Player");	
+			System.out.println("2. Delete Player");
+			System.out.println("3. Edit Player");
+			System.out.println("4. View Player");
+			System.out.println("5. Show a menu");
+			System.out.println("6. Exit");
+			System.out.println("Select one number between 1-6:");
+			num = input.nextInt();
+			
+			switch(num) {
+				case 1: {
+					System.out.print("Player Number to add: ");
+					int PlayerNumber1= input.nextInt();
+					System.out.print("Player Name: ");
+					String PlayerName= input.next();
+					break;
+					}
+				case 2:
+					System.out.print("Player Number to delete: ");
+					int PlayerNumber2= input.nextInt();
+					break;
+				case 3:
+					System.out.print("Player Number to edit: ");
+					int PlayerNumber3= input.nextInt();
+					break;
+				case 4:
+					System.out.print("Player Number to view: ");
+					int PlayerNumber4= input.nextInt();
+					break;
+				case 5:
+					
+					break;
+				case 6:
+					
+					break;
+				default:
+					System.out.println("Plaese enter a number between 1-6!");
+					break;
+			}
+		}
 
 	}
 
